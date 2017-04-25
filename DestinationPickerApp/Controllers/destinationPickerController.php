aee6bbd518e062cb444ba1748e397e4a140527fa
David <d.c.s.lee-09@alumni.lboro.ac.uk>
2017-04-25 17:54:21 +0100

    Removed syntax error

--- DestinationPickerApp/Controllers/destinationPickerController.php
+++ DestinationPickerApp/Controllers/destinationPickerController.php
@@ -3,7 +3,7 @@
 include("destinationPickerClass.php");
 include("destinationPickerView.php");
 
-$destinationController = new destinationPickerClass($_SESSION["user"], $_GET["destination"])
+$destinationController = new destinationPickerClass($_SESSION["user"], $_GET["destination"]);
 $view = new View($destinationObject);
 echo $view->output();
 
--- DestinationPickerApp/Models/destinationModel.php
+++ DestinationPickerApp/Models/destinationModel.php
@@ -35,6 +35,7 @@ class Destination{
         //retreive destinations from the database
 
     }
+
 }
 
 
